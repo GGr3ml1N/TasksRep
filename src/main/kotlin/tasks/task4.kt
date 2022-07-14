c963acf88d33e3b71fcd8f0264129f7e977eee62
GGr3ml1N <dinars163@gmail.com>
2022-07-14 12:16:37 +0300

    task4 completed

---
+++ src/main/kotlin/tasks/task4.kt
@@ -0,0 +1,26 @@
+package tasks
+
+/**
+ * Число-палиндром с обеих сторон (справа налево и слева направо) читается одинаково. Самое большое число-палиндром, полученное умножением двух двузначных чисел – 9009 = 91 × 99.
+
+Найдите самый большой палиндром, полученный умножением двух трехзначных чисел.
+ */
+fun main() {
+    println(numP())
+}
+
+fun numP(): Int {
+    var result = 0
+    var flag = true
+    var i = 999
+    var j = 999
+    while (flag) {
+        result = i * j
+        i--
+        j--
+        if (result == result.toString().reversed().toInt()) {
+            flag = false
+        }
+    }
+    return result
+}
